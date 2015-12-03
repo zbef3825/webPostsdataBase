f78a333fd6c9dd1400731277ede0880cf2b826d5
Jinwook Lee <jinwook.lee365@gmail.com>
2015-12-02 23:49:50 -0800

    changed app.js to server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 //importing express framework
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 //setting up local port or current environment port
 
-var database = require(__dirname +'/database.js');
+var database = require('./database.js');
 
 database(app);
 
 
-app.listen(port);
+app.listen(PORT);
 //starting a server
