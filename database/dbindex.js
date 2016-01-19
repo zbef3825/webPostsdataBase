bfa46508286a9a86b2a97d1aa8306183a738b65b
Jinwook Lee <jinwook.lee365@gmail.com>
2016-01-18 17:38:24 -0800

    Added access control allow methods: get and Delete

--- database/dbindex.js
+++ database/dbindex.js
@@ -16,6 +16,8 @@ module.exports = function(app) {
 	app.use(function(req, res, next) {
 	//COR handler
 		res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET");
+        res.header("Access-Control-Allow-Methods", "DELETE");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
 	});
